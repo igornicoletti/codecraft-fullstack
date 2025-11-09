bcf14f6e83813722c5ac6b87ddd1b5fdb5101686
igornicoletti <igor93nicoletti@gmail.com>
2025-11-09 14:40:46 -0300

    style(ui): add font-medium to animated shiny text

--- src/components/demo/HeroSectionOne.tsx
+++ src/components/demo/HeroSectionOne.tsx
@@ -6,17 +6,17 @@ import { ArrowElbowRightDownIcon, LightningIcon, MailboxIcon } from '@phosphor-i
 export const HeroSectionOne = () => {
   return (
     <div id='hero' className='relative mx-auto w-full max-w-7xl overflow-hidden py-16 pb-96 md:py-32'>
-      <div className='flex flex-col items-center text-center gap-4 px-6'>
+      <div className='flex flex-col items-center text-center gap-6 px-8'>
         <AnimatedShinyText>
           <span>Igor Nicoletti</span>
           <LightningIcon weight='thin' className='text-primary' />
           <span>Desenvolvedor FullStack</span>
         </AnimatedShinyText>
-        <h1 className="font-extralight leading-tighter tracking-tight text-muted-foreground text-4xl md:text-5xl lg:text-6xl xl:text-7xl">
+        <h1 className="font-extralight leading-tighter tracking-tight text-muted-foreground text-4xl sm:text-5xl lg:text-6xl xl:text-7xl">
           Experiências web <br className='sm:hidden' />
           <FlipWords words={["modernas", "eficientes", "escaláveis"]} className='font-light text-5xl lg:text-6xl xl:text-7xl' />
         </h1>
-        <p className="max-w-2xl text-base text-muted-foreground md:text-lg">
+        <p className="text-base lg:text-lg xl:text-xl text-balance text-muted-foreground">
           Lorem ipsum dolor sit amet. Sed sint voluptatibus est dicta reprehenderit rem eveniet magni. Et quia earum rem autem animi et inventore corporis ut illum voluptates sed.
         </p>
         <div className="mt-8 w-full flex flex-wrap items-center justify-center gap-4">
