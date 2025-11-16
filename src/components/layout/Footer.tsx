5b506f2dfd2bee6c488074e7ea33f399fe83ccfb
igornicoletti <igor93nicoletti@gmail.com>
2025-11-16 11:35:56 -0300

    feat(components): carousel ExperienceSection component

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => (
   <footer className='border-t px-6 py-8'>
-    <p className='text-sm text-center font-light'>
-      &copy; {new Date().getFullYear()} CodeCraft
+    <p className='text-sm text-center text-muted-foreground font-light'>
+      &copy; {new Date().getFullYear()} CodeCraft. Todos os direitos reservados.
     </p>
   </footer>
 )
