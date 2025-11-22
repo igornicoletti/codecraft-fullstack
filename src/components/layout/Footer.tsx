e545d4e82411f33f52f6c609b14f9b6b982ec303
igornicoletti <igor93nicoletti@gmail.com>
2025-11-22 03:11:37 -0300

    feat(components): hero

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -4,14 +4,14 @@ export const Footer = () => {
   const currentYear = new Date().getFullYear()
 
   return (
-    <footer className="relative overflow-hidden py-12 md:pt-36">
+    <footer className="relative overflow-hidden py-16 md:pt-40">
       {/* Background effect */}
       <div className="absolute bottom-2 w-full translate-y-0 sm:translate-y-1/2">
         <TextHoverEffect text="CodeCraft" />
       </div>
       {/* Copyright content */}
       <div className="relative container mx-auto px-6 text-center">
-        <p className="text-xs text-muted-foreground md:text-sm">
+        <p className="text-xs text-secondary md:text-base">
           &copy; {currentYear} CodeCraft. Todos os direitos reservados.
         </p>
       </div>
