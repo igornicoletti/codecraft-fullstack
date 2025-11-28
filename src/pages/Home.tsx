cec53348136dfd5c25c7cdb9a056b0f3dcfeadcb
igornicoletti <igor93nicoletti@gmail.com>
2025-11-28 14:20:59 -0300

    feat: update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,9 +3,11 @@ import { HeroSection } from '@/components/features/HeroSection'
 import { ProjectSection } from '@/components/features/ProjectSection'
 import { Footer } from '@/components/layout/Footer'
 import { Navbar } from '@/components/layout/Navbar'
+import { AnimatedWavePath } from '@/components/ui/animated-wave-path'
 
 export const Home = () => (
   <div id='app'>
+    <AnimatedWavePath />
     <Navbar />
     <HeroSection />
     <ExperienceSection />
