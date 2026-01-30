c261c17dca63527ac445646ee3edaa49c85a4eaa
igornicoletti <igor93nicoletti@gmail.com>
2026-01-29 21:59:25 -0300

    feat(vercel): updated

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { ThemeProvider } from '@/contexts/ThemeProvider'
 import { Home } from '@/pages/Home'
 
 export const App = () => (
-  <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+  <ThemeProvider defaultTheme='light' storageKey='vite-ui-theme'>
     <Home />
   </ThemeProvider>
 )
--- src/constants/project.ts
+++ src/constants/project.ts
@@ -1,7 +1,7 @@
 import type { ProjectSectionContent } from '@/types/project.types'
 
 export const projectSectionData: ProjectSectionContent = {
-  sectionTagline: 'Projetos open source',
+  sectionTagline: 'Projetos consolidados',
   mainHeadline: 'Códigos que falam por si só',
   sectionSummary: 'Explore uma coleção selecionada de projetos que mostram conceitos de aplicações web eficientes, utilizando tecnologias modernas e melhores práticas de desenvolvimento.',
   externalAction: {
@@ -17,33 +17,33 @@ export const projectSectionData: ProjectSectionContent = {
     },
     {
       projectTitle: 'CodeCraft Auth',
-      shortDescription: 'Uma aplicação web de autenticação moderna e pronta para produção, construída com **Vite + React + TypeScript + Supabase**, seguindo as convenções globais da indústria para estrutura de pastas, ferramentas, testes e escalabilidade.',
-      liveDemoUrl: 'https://codecraft-auth.vercel.app/',
-      repositoryUrl: 'https://github.com/igornicoletti/codecraft-auth',
-    },
-    {
-      projectTitle: 'CodeCraft FullStack',
       shortDescription: 'Lorem ipsum dolor sit amet. Est officia esse et illo libero aut esse galisum id modi omnis non perspiciatis cumque? Qui odio asperiores et officiis veniam et aperiam voluptatum?',
-      liveDemoUrl: 'https://codecraft-fullstack.vercel.app/',
-      repositoryUrl: 'https://github.com/igornicoletti/codecraft-fullstack',
-    },
-    {
-      projectTitle: 'CodeCraft Auth',
-      shortDescription: 'Uma aplicação web de autenticação moderna e pronta para produção, construída com **Vite + React + TypeScript + Supabase**, seguindo as convenções globais da indústria para estrutura de pastas, ferramentas, testes e escalabilidade.',
       liveDemoUrl: 'https://codecraft-auth.vercel.app/',
       repositoryUrl: 'https://github.com/igornicoletti/codecraft-auth',
     },
     {
-      projectTitle: 'CodeCraft FullStack',
+      projectTitle: 'Padocaria SP',
       shortDescription: 'Lorem ipsum dolor sit amet. Est officia esse et illo libero aut esse galisum id modi omnis non perspiciatis cumque? Qui odio asperiores et officiis veniam et aperiam voluptatum?',
-      liveDemoUrl: 'https://codecraft-fullstack.vercel.app/',
-      repositoryUrl: 'https://github.com/igornicoletti/codecraft-fullstack',
+      liveDemoUrl: 'https://concurso.padocariasp.com.br/',
+      repositoryUrl: 'https://concurso.padocariasp.com.br/',
     },
     {
-      projectTitle: 'CodeCraft Auth',
-      shortDescription: 'Uma aplicação web de autenticação moderna e pronta para produção, construída com **Vite + React + TypeScript + Supabase**, seguindo as convenções globais da indústria para estrutura de pastas, ferramentas, testes e escalabilidade.',
-      liveDemoUrl: 'https://codecraft-auth.vercel.app/',
-      repositoryUrl: 'https://github.com/igornicoletti/codecraft-auth',
+      projectTitle: 'Colic Calm Brasil',
+      shortDescription: 'Lorem ipsum dolor sit amet. Est officia esse et illo libero aut esse galisum id modi omnis non perspiciatis cumque? Qui odio asperiores et officiis veniam et aperiam voluptatum?',
+      liveDemoUrl: 'https://coliccalmbrasil.com.br/',
+      repositoryUrl: 'https://coliccalmbrasil.com.br/',
+    },
+    {
+      projectTitle: 'Ipê – Instituto de Pesquisas Ecológicas',
+      shortDescription: 'Lorem ipsum dolor sit amet. Est officia esse et illo libero aut esse galisum id modi omnis non perspiciatis cumque? Qui odio asperiores et officiis veniam et aperiam voluptatum?',
+      liveDemoUrl: 'https://ipe.org.br/',
+      repositoryUrl: 'https://ipe.org.br/',
+    },
+    {
+      projectTitle: 'Hubchat',
+      shortDescription: 'Lorem ipsum dolor sit amet. Est officia esse et illo libero aut esse galisum id modi omnis non perspiciatis cumque? Qui odio asperiores et officiis veniam et aperiam voluptatum?',
+      liveDemoUrl: 'https://www.hubchat.com.br/',
+      repositoryUrl: 'https://www.hubchat.com.br/',
     },
   ]
 }
